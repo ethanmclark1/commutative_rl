320c610b1bc54ec4b89e8cf010a05cc1508066bc
Ethan <eclark715@gmail.com>
2024-05-13 12:15:26 -0700

    Modified speaker to match updated main

--- ma-cdl/agents/speaker.py
+++ ma-cdl/agents/speaker.py
@@ -4,41 +4,37 @@ from shapely import Point
 from languages.utils.cdl import CDL
 
 class Speaker:
-    def __init__(self, num_agents, obstacle_radius):
+    def __init__(self, num_agents: int, obstacle_radius: float) -> None:
         self.num_agents = num_agents
         self.obstacle_radius = obstacle_radius
     
     # Determine the positions of the agents, goals, and obstacles
-    def gather_info(self, state):
-        self.agents = []
+    def gather_info(self, state) -> None:
+        self.starts = []
         self.goals = []
         self.obstacles = None
         
         for idx in range(self.num_agents):
-            self.agents += [state[idx*2 : idx*2+2]]
-            self.goals += [state[self.num_agents*2 + idx*2 : self.num_agents*2 + idx*2 + 2]]
+            self.starts += [state[2*idx : 2*idx+2]]
+            self.goals += [state[2*self.num_agents + 2*idx : 2*self.num_agents + 2*idx + 2]]
             
-        self.obstacles = state[self.num_agents*4 : ].reshape(-1, 2)
+        self.obstacles = state[4*self.num_agents : ].reshape(-1, 2)
     
-    # Get the directions for each agent
-    def direct(self, approach):
-        directions = []
-        if isinstance(approach, list):
+    def direct(self, name: str, approach: object) -> list:
+        if any(x in name for x in ['dqn', 'sac', 'voronoi_map']):
             obstacles = [Point(obstacle).buffer(self.obstacle_radius) for obstacle in self.obstacles]
 
-            for agent, goal in zip(self.agents, self.goals):
-                agent_idx = CDL.localize(agent, approach)
-                goal_idx = CDL.localize(goal, approach)
-                
-                safe_graph = CDL.get_safe_graph(approach, obstacles)
+            directions = []
+            for start, goal in zip(self.starts, self.goals):
+                graph, start_region, goal_region = CDL.create_instance(approach, start, goal, obstacles)
                 
                 try:
-                    directions += [nx.astar_path(safe_graph, agent_idx, goal_idx)]
+                    directions += [nx.astar_path(graph, start_region, goal_region)]
                 except (nx.NodeNotFound, nx.NetworkXNoPath):
-                    directions += [None]
-            
+                    directions += [None]        
+        elif name == 'grid_world':
+            directions = [approach.direct(start, goal, self.obstacles) for start, goal in zip(self.starts, self.goals)]
         else:
-            for agent, goal in zip(self.agents, self.goals):
-                directions += [approach.direct(agent, goal, self.obstacles)]
+            directions = [approach]
         
         return directions
