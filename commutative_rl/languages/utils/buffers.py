ec087a6d7b7f5b5f0f4c15e873eb3aeef68b72e6
Ethan <eclark715@gmail.com>
2024-08-06 22:22:30 -0700

    update signal8 to conform to set optimizer v1

--- commutative_rl/languages/utils/buffers.py
+++ commutative_rl/languages/utils/buffers.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 
 
+torch.set_default_dtype(torch.float32)
+
 class ReplayBuffer:
     def __init__(self, 
                  seed: int,
@@ -19,21 +21,14 @@ class ReplayBuffer:
         self.action_dims = action_dims
         self.action_dtype = torch.int64 if action_size == 1 else torch.float
         
-        self.prev_state = torch.zeros(buffer_size, state_size, dtype=torch.float)
-        self.prev_action = torch.zeros(buffer_size, action_size, dtype=self.action_dtype)
-        
-        self.state = torch.zeros(buffer_size, state_size, dtype=torch.float)
+        self.state = torch.zeros(buffer_size, state_size)
         self.action = torch.zeros(buffer_size, action_size, dtype=self.action_dtype)
-        self.reward = torch.zeros(buffer_size, 1, dtype=torch.float)
-        self.next_state = torch.zeros(buffer_size, state_size, dtype=torch.float)
+        self.reward = torch.zeros(buffer_size, 1)
+        self.next_state = torch.zeros(buffer_size, state_size)
         self.done = torch.zeros(buffer_size, 1, dtype=torch.bool)
-        self.num_action = torch.zeros(buffer_size, 1, dtype=torch.torch.float)
+        self.num_action = torch.zeros(buffer_size, 1)
         self.is_initialized = torch.zeros(buffer_size, dtype=torch.bool)
-        
-        self.has_commutative = torch.zeros(buffer_size, dtype=torch.bool)
-        self.commutative_reward = torch.zeros(buffer_size, 1, dtype=torch.float)
 
-        # Managing buffer size and current position
         self.count = 0
         self.real_size = 0
         self.size = buffer_size
@@ -50,9 +45,6 @@ class ReplayBuffer:
             next_state: list,
             done: bool,
             num_action: int,
-            prev_state: list = None,
-            prev_action: int = None,
-            commutative_reward: float = None
             ) -> None:
         
         # Encode all values
@@ -69,12 +61,6 @@ class ReplayBuffer:
         self.done[self.count] = torch.as_tensor(done)
         self.num_action[self.count] = torch.as_tensor(num_action)
         self.is_initialized[self.count] = True
-        
-        if commutative_reward is not None:        
-            self.prev_state[self.count] = torch.as_tensor(prev_state)
-            self.prev_action[self.count] = torch.as_tensor(prev_action)
-            self.commutative_reward[self.count] = torch.as_tensor(commutative_reward)
-            self.has_commutative[self.count] = True
 
         self._increase_size()
 
@@ -93,8 +79,8 @@ class RewardBuffer:
                  max_action: int=None,
                  ) -> None:
                
-        self.transition = torch.zeros((buffer_size, step_dims), dtype=torch.float) 
-        self.reward = torch.zeros((buffer_size, 1), dtype=torch.float)
+        self.transition = torch.zeros((buffer_size, step_dims)) 
+        self.reward = torch.zeros((buffer_size, 1))
         self.is_initialized = torch.zeros((buffer_size), dtype=torch.bool)
         
         self.action_dtype = torch.int64 if action_dims else torch.float
@@ -156,7 +142,7 @@ class CommutativeRewardBuffer(RewardBuffer):
                  ) -> None:
         
         super().__init__(seed, buffer_size, step_dims, action_dims, max_action)
-        self.transition = torch.zeros((buffer_size, 2, step_dims), dtype=torch.float)
+        self.transition = torch.zeros((buffer_size, 2, step_dims))
         
     def add(self, 
             prev_state: list,
--- commutative_rl/languages/utils/networks.py
+++ commutative_rl/languages/utils/networks.py
@@ -10,15 +10,21 @@ class RewardEstimator(nn.Module):
         super(RewardEstimator, self).__init__()
         torch.manual_seed(seed)
         
-        self.fc1 = nn.Linear(in_features=input_dims, out_features=16)
-        self.fc2 = nn.Linear(in_features=16, out_features=1)
+        fc_output_dims = 32
+        
+        self.fc1 = nn.Linear(in_features=input_dims, out_features=fc_output_dims)
+        self.ln1 = nn.LayerNorm(fc_output_dims)
+        self.fc2 = nn.Linear(in_features=fc_output_dims, out_features=fc_output_dims)
+        self.ln2 = nn.LayerNorm(fc_output_dims)
+        self.fc3 = nn.Linear(in_features=fc_output_dims, out_features=1)
         
         self.loss = nn.MSELoss()
         self.optim = Adam(self.parameters(), lr=lr)
         
-    def forward(self, x: torch.tensor) -> torch.tensor:
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.selu(self.ln1(self.fc1(x)))
+        x = F.selu(self.ln2(self.fc2(x)))
+        return self.fc3(x)
     
 
 class DQN(nn.Module):
@@ -33,7 +39,7 @@ class DQN(nn.Module):
         self.loss = nn.MSELoss()
         self.optim = Adam(self.parameters(), lr=lr)
         
-    def forward(self, state: torch.tensor, num_action: torch.tensor) -> torch.tensor:
+    def forward(self, state: torch.Tensor, num_action: torch.Tensor) -> torch.Tensor:
         x = torch.cat((state, num_action), dim=-1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
