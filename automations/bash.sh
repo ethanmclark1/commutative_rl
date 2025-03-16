f40f2b05c327e34a746541666ebd57f64fc99dbe
Ethan Clark <eclark715@gmail.com>
2025-03-15 20:24:01 -0700

    create automations

---
+++ automations/bash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SEED=(42)
+N_INSTANCES=5
+APPROACHES=("QTable" "TripleDataQTable" "SuperActionQTable" "CombinedRewardQTable" "HashMapQTable")
+
+ALPHA=(0.01)
+EPSILON=(0.25) 
+GAMMA=(0.99)
+
+conda activate commutative_rl
+
+total_jobs=$(( ${#SEED[@]} * (N_INSTANCES) * ${#APPROACHES[@]} * ${#ALPHA[@]} * ${#EPSILON[@]} * ${#GAMMA[@]} ))
+
+current_job=0
+for seed in "${SEED[@]}"; do
+    for problem_instance in $(seq 0 $((N_INSTANCES - 1))); do
+        for approach in "${APPROACHES[@]}"; do
+            for alpha in "${ALPHA[@]}"; do
+                for epsilon in "${EPSILON[@]}"; do
+                    for gamma in "${GAMMA[@]}"; do
+                        current_job=$((current_job + 1))
+
+                        echo "Starting job $current_job of $total_jobs"
+
+                        args="--approaches $approach --seed $seed --problem_instances instance_$problem_instance --alpha $alpha --epsilon $epsilon --gamma $gamma"                                    
+                        python commutative_rl/main.py $args 
+                        echo "Completed job $current_job: $job_name"
+                    done
+                done
+            done
+        done
+    done
+done
+
+echo "All jobs completed successfully!"
--- automations/slurm.sh
+++ automations/slurm.sh
@@ -2,22 +2,19 @@
 
 SEED=(42)
 N_INSTANCES=5
-APPROACHES=("QTable" "TripleDataDQN" "DoubleTableQTable" "CombinedRewardQTable" "HashMapQTable")
+APPROACHES=("QTable" "TripleDataQTable" "SuperActionQTable" "CombinedRewardQTable" "HashMapQTable")
 
 GRID_DIMS=("12x12")
 N_STARTS=(3)
 N_GOALS=(3)
 N_BRIDGES=(15)
-N_EPISODE_STEPS=(100)
+N_EPISODE_STEPS=(300)
 ACTION_SUCCESS_RATE=(0.65)
-UTILITY_SCALE=(25)
-TERMINAL_REWARD=(50)
-BRIDGE_COST_LB=(10)
-BRIDGE_COST_UB=(30)
-DUPLICATE_BRIDGE_PENALTY=(150)
+UTILITY_SCALE=(30)
+TERMINAL_REWARD=(25)
 
-ALPHA=(0.001 0.01 0.1)   
-EPSILON=(0.25 0.30)
+ALPHA=(0.01)   
+EPSILON=(0.25)
 GAMMA=(0.99)
 
 TMP_DIR=$(mktemp -d)
@@ -35,11 +32,10 @@ generate_job_name() {
     local action_success_rate=$9
     local utility_scale=${10}
     local terminal_reward=${11}
-    local duplicate_bridge_penalty=${12}
-    local alpha=${13}
-    local epsilon=${14}
-    local gamma=${15}
-    echo "${approach}_${seed}_${problem_instance}_${grid_dims}_${n_starts}_${n_goals}_${n_bridges}_${n_episode_steps}_${action_success_rate}_${utility_scale}_${terminal_reward}_${duplicate_action_cost}_${alpha}_${epsilon}_${gamma}.sh"
+    local alpha=${12}
+    local epsilon=${13}
+    local gamma=${14}
+    echo "${approach}_${seed}_${problem_instance}_${grid_dims}_${n_starts}_${n_goals}_${n_bridges}_${n_episode_steps}_${action_success_rate}_${utility_scale}_${terminal_reward}_${alpha}_${epsilon}_${gamma}.sh"
 }
 
 for seed in "${SEED[@]}"; do
@@ -53,18 +49,17 @@ for seed in "${SEED[@]}"; do
                                 for action_success_rate in "${ACTION_SUCCESS_RATE[@]}"; do
                                     for utility_scale in "${UTILITY_SCALE[@]}"; do
                                         for terminal_reward in "${TERMINAL_REWARD[@]}"; do
-                                            for duplicate_bridge_penalty in "${DUPLICATE_BRIDGE_PENALTY[@]}"; do
-                                                for alpha in "${ALPHA[@]}"; do
-                                                    for epsilon in "${EPSILON[@]}"; do
-                                                        for gamma in "${GAMMA[@]}"; do
-                                                            args="--approaches $approach --seed $seed --problem_instances instance_$problem_instance --grid_dims $grid_dims --n_starts $n_starts --n_goals $n_goals --n_bridges $n_bridges --n_episode_steps $n_episode_steps --action_success_rate $action_success_rate --utility_scale $utility_scale --terminal_reward $terminal_reward --duplicate_bridge_penalty $duplicate_bridge_penalty --alpha $alpha --epsilon $epsilon --gamma $gamma"
-                                                            job_name=$(generate_job_name "$approach" "$seed" "$problem_instance" "$grid_dims" "$n_starts" "$n_goals" "$n_bridges" "$n_episode_steps" "$action_success_rate" "$utility_scale" "$terminal_reward" "$duplicate_bridge_penalty" "$alpha" "$epsilon" "$gamma")
-                                                            cat <<EOT > "$TMP_DIR/$job_name"
+                                            for alpha in "${ALPHA[@]}"; do
+                                                for epsilon in "${EPSILON[@]}"; do
+                                                    for gamma in "${GAMMA[@]}"; do
+                                                        args="--approaches $approach --seed $seed --problem_instances instance_$problem_instance --grid_dims $grid_dims --n_starts $n_starts --n_goals $n_goals --n_bridges $n_bridges --n_episode_steps $n_episode_steps --action_success_rate $action_success_rate --utility_scale $utility_scale --terminal_reward $terminal_reward --alpha $alpha --epsilon $epsilon --gamma $gamma"
+                                                        job_name=$(generate_job_name "$approach" "$seed" "$problem_instance" "$grid_dims" "$n_starts" "$n_goals" "$n_bridges" "$n_episode_steps" "$action_success_rate" "$utility_scale" "$terminal_reward" "$alpha" "$epsilon" "$gamma")
+                                                        cat <<EOT > "$TMP_DIR/$job_name"
 #!/bin/bash
 
 #SBATCH -N 1                               # Number of nodes
 #SBATCH -c 32                              # Number of cores
-#SBATCH -t 0-01:00:00                      # time in d-hh:mm:ss
+#SBATCH -t 0-04:00:00                      # time in d-hh:mm:ss
 #SBATCH -p htc                             # partition
 #SBATCH -q public                          # QOS
 #SBATCH -o artifacts/${job_name%.sh}.out   # file to save job's STDOUT (%j = JobID)
@@ -77,7 +72,6 @@ source activate commutative_rl
 python commutative_rl/main.py $args
 
 EOT
-                                                        done        
                                                     done
                                                 done
                                             done
